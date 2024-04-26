21eaeb248a0f154331c3720ca125580ef665bd27
Jayden Li <96189395+x-I8V@users.noreply.github.com>
2024-04-26 18:46:48 -0400

    Blanked script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const comingSoonElements = document.querySelectorAll(".coming-soon");
 
-    comingSoonElements.forEach(element => {
-        element.addEventListener("click", function() {
-            alert("Coming soon!");
-        });
-    });
-});
